075a397a2ae7d8239adb636e5dd6f48d2c5c3023
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2022-01-25 20:32:11 -0800

    adding cts into it

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/aws-consul-primary/outputs.tf
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/aws-consul-primary/outputs.tf
@@ -5,3 +5,11 @@ output "aws_consul_public_ip" {
 output "aws_mgw_public_ip" {
   value = aws_instance.mesh_gateway.public_ip
 }
+
+output "cts-id" {
+    value = "${aws_instance.cts.id}"
+  }
+
+output "cts-publicip" {
+    value = "${aws_instance.cts.public_ip}"
+  }
